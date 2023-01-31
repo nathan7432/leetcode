5064445378f82da0e586f280a09cc4f5977610e5
Nathan Pirhalla <steelersfan5052@yahoo.com>
2023-01-31 14:13:12 -0500

    Create 226_InvertBinaryTree.js

---
+++ 226_InvertBinaryTree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root === null) {
+        return null
+    }
+    var temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    if (root.left !== null) {
+        invertTree(root.left)
+    }
+    if (root.right !== null) {
+        invertTree(root.right)
+    }
+    return root
+};
